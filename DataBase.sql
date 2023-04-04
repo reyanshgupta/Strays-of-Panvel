b402e0d06be2ddc4fc581ed662d72f919f3c740f
Reyansh Gupta <happygupta@aol.in>
2023-04-04 18:30:51 +0530

    1830

--- DataBase.sql
+++ DataBase.sql
@@ -1,3 +1,11 @@
 CREATE TABLE `straysofpanvel`.`adoptionrequests` (`Name` VARCHAR(30) NOT NULL , `PhoneNumber` VARCHAR(15) NOT NULL , `Email` VARCHAR(25) NOT NULL , `Address` VARCHAR(100) NOT NULL , `PetName` VARCHAR(15) NOT NULL , `PetAge` INT(5) NOT NULL , `PetBreed` VARCHAR(20) NOT NULL , `Gender` VARCHAR(10) NOT NULL ) ENGINE = InnoDB;
 CREATE TABLE `straysofpanvel`.`adminlogin` (`admin_id` VARCHAR(25) NOT NULL , `password` VARCHAR(25) NOT NULL , PRIMARY KEY (`admin_id`)) ENGINE = InnoDB;
 ALTER TABLE `adoptionrequests` ADD `req_id` INT(30) NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (`req_id`);
+ALTER TABLE `adoptionrequests` ADD `lastName` VARCHAR(30) NOT NULL AFTER `Name`;
+ALTER TABLE `adoptionrequests` CHANGE `Name` `firstName` VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL;
+ALTER TABLE `adoptionrequests` CHANGE `Address` `primAddress` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL;
+
+
+
+
+CREATE TABLE `StraysOfPanvel`.`adoptionrequests` (`req_id` INT(30) NOT NULL AUTO_INCREMENT , `firstName` VARCHAR(30) NOT NULL , `lastName` VARCHAR(30) NOT NULL , `PhoneNumber` INT(15) NOT NULL , `Email` VARCHAR(30) NOT NULL , `primAddress` VARCHAR(255) NOT NULL , `PetName` VARCHAR(30) NOT NULL , `PetAge` INT(20) NOT NULL , `PetBreed` VARCHAR(30) NOT NULL , `Gender` VARCHAR(10) NOT NULL , PRIMARY KEY (`req_id`)) ENGINE = InnoDB;
