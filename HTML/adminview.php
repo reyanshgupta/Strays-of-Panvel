8ae1737255a5de995b24fe472d3fbbb63163bda4
Reyansh Gupta <happygupta@aol.in>
2023-04-04 19:12:04 +0530

    1912

--- HTML/adminview.php
+++ HTML/adminview.php
@@ -7,7 +7,61 @@
         <link rel="stylesheet" type="text/css" href="../CSS/imagegallery.css">
         <link rel="stylesheet" type="text/css" href="../CSS/animations.css">
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.8.2/angular.min.js"></script>
-    </head>
+        <style>
+            table {
+            border-collapse: collapse;
+            width: 100%;
+            margin: 20px 0;
+            font-size: 1em;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+            }
+
+            th {
+            background-color: black;
+            color: #ffffff;
+            text-align: left;
+            font-weight: bold;
+            padding: 12px;
+            text-transform: uppercase;
+            letter-spacing: 1px;
+            }
+
+            td {
+            border-bottom: 1px solid #f2f2f2;
+            padding: 12px;
+            }
+
+            tr:hover {
+            background-color: #f5f5f5;
+            }
+
+            @media screen and (max-width: 600px) {
+            table {
+                border: 0;
+            }
+            table thead {
+                display: none;
+            }
+            table tr {
+                margin-bottom: 10px;
+                display: block;
+                box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+            }
+            table td {
+                border-bottom: none;
+                position: relative;
+                padding-left: 50%;
+            }
+            table td:before {
+                position: absolute;
+                top: 12px;
+                left: 6px;
+                width: 45%;
+                padding-right: 10px;
+                white-space: nowrap;
+            }
+    </style>
+</head>
 <body>
 <header>
     <a href="homepage.html"><img class="center" style="margin-bottom: 1%;" src="../Images/SOPLogo.jpg" width="7%"></a>
@@ -30,6 +84,7 @@
         $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
         mysqli_close($conn); 
     ?>
+    <h2>Current Adoption Requests: </h2>
     <table>
         <tr>
             <th>First Name</th>
@@ -37,19 +92,26 @@
             <th>Phone Number</th>
             <th>Email</th>
             <th>Address</th>
-            <th>Last Name</th>
-            <th>Last Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
+            <th>Pet Name</th>
+            <th>Pet Age</th>
+            <th>Pet Breed</th>
+            <th>Gender</th>
         </tr>
         <?php foreach ($rows as $row): ?>
         <tr>
-            <td><?php echo $row['id']; ?></td>
-            <td><?php echo $row['name']; ?></td>
-            <td><?php echo $row['email']; ?></td>
+            <td><?php echo $row['firstName']; ?></td>
+            <td><?php echo $row['lastName']; ?></td>
+            <td><?php echo $row['PhoneNumber']; ?></td>
+            <td><?php echo $row['Email']; ?></td>
+            <td><?php echo $row['PrimAddress']; ?></td>
+            <td><?php echo $row['PetName']; ?></td>
+            <td><?php echo $row['PetAge']; ?></td>
+            <td><?php echo $row['PetBreed']; ?></td>
+            <td><?php echo $row['Gender']; ?></td>
         </tr>
         <?php endforeach; ?>
     </table>
+<hr style="border: none; border-top: 4px solid black; margin: 20px 0;">
 <footer>
     <p>&copy; 2023 Strays of Panvel. All Rights Reserved.</p>
     <ul>
